e742d1b4703199a4bd1e82f4d5e053454407a7b1
jean-phi66 <jean.philippe.navarro@gmail.com>
2017-11-15 22:06:30 +0100

    Naive Bayes miniproject completed

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python
 
-""" 
-    This is the code to accompany the Lesson 1 (Naive Bayes) mini-project. 
+"""
+    This is the code to accompany the Lesson 1 (Naive Bayes) mini-project.
 
     Use a Naive Bayes Classifier to identify emails by their authors
-    
+
     authors and labels:
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
 from time import time
 sys.path.append("../tools/")
@@ -27,7 +27,28 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 ### your code goes here ###
 
+# Discover training and test set dimensions
+print "features_train:", type(features_train), features_train.shape
+print "features_test: ", type(features_test), features_test.shape
+print "labels_train:", type(labels_train), len(labels_train)
+print "labels_test: ", type(labels_test), len(labels_test)
+print ""
+
+# import useful function from sklearn
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
+
+# Naive Bayes Classifier
+clf = GaussianNB()
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+t0 = time()
+pred = clf.predict(features_test)
+print "preidct time:", round(time()-t0, 3), "s"
+print ""
+
+print "accuracy:", accuracy_score(labels_test, pred)
+
 
 #########################################################
-
-
