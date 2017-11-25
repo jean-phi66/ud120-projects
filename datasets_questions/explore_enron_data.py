2e8fde754448b2875668a49f6c86eef709b40c6a
jean-phi66 <jean.philippe.navarro@gmail.com>
2017-11-25 20:49:46 +0100

    DataSet and questions python code modified to answer questions

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-""" 
+"""
     Starter code for exploring the Enron dataset (emails + finances);
     loads up the dataset (pickled dict of dicts).
 
@@ -12,11 +12,57 @@
     but here's an example to get you started:
 
     enron_data["SKILLING JEFFREY K"]["bonus"] = 5600000
-    
+
 """
 
 import pickle
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
+print "Enron Dataset type", type(enron_data)
+print "Number of data points", len(enron_data)
+print "Number of features per person", len(enron_data['ELLIOTT STEVEN'])
+# count number of POI
+cpt = 0
+for k,v in enron_data.iteritems():
+    if v['poi'] :
+        cpt += 1
+print "Number of POI in dataset:", cpt
+print("names", enron_data.keys())
+print enron_data['PRENTICE JAMES']['total_stock_value']
+print enron_data['COLWELL WESLEY']['from_this_person_to_poi']
+print "Skilling :", enron_data['SKILLING JEFFREY K']
+names = ['FASTOW ANDREW S', 'SKILLING JEFFREY K', 'LAY KENNETH L']
+for name in names:
+    print name, enron_data[name]['total_payments']
+cpt = 0
+for k,v in enron_data.iteritems():
+    if v['salary'] != 'NaN':
+        cpt += 1
+print 'number with salary', cpt
+cpt = 0
+for k,v in enron_data.iteritems():
+    if v['email_address'] != 'NaN':
+        cpt += 1
+print 'number with email_address', cpt
+def missing_data(enron_data, data_name):
+    cpt = 0
+    for k,v in enron_data.iteritems():
+        if v[data_name] == 'NaN':
+            cpt += 1
+    print 'missing', data_name, ':', cpt,'=>', float(cpt)/len(enron_data)*100, '%'
+    return cpt
+
+def missing_data_poi(enron_data, data_name):
+    cpt = 0
+    for k,v in enron_data.iteritems():
+        if v[data_name] == 'NaN':
+            if v['poi'] : cpt += 1
+    print 'missing poi', data_name, ':', cpt,'=>', float(cpt)/len(enron_data)*100, '%'
+    return cpt
+
+missing_data(enron_data, 'total_payments')
+missing_data_poi(enron_data, 'total_payments')
 
 
+
+#['exercised_stock_options']
