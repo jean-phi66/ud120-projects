b7d7e30d5a9ddfa6aac49e86da8f9b2aae075e81
jean-phi66 <jean.philippe.navarro@gmail.com>
2017-11-26 08:41:32 +0100

    Outliers mini-project completed

--- outliers/enron_outliers.py
+++ outliers/enron_outliers.py
@@ -10,10 +10,28 @@ from feature_format import featureFormat, targetFeatureSplit
 ### read in data dictionary, convert to numpy array
 data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r") )
 features = ["salary", "bonus"]
+data_dict.pop( 'TOTAL', 0 )
 data = featureFormat(data_dict, features)
 
 
 ### your code below
+maxi = 0
+for point in data:
+    salary = point[0]
+    bonus = point[1]
+    if salary > maxi:
+        maxi = salary
+    matplotlib.pyplot.scatter( salary, bonus )
 
+matplotlib.pyplot.xlabel("salary")
+matplotlib.pyplot.ylabel("bonus")
+matplotlib.pyplot.show()
 
+for k,v in data_dict.iteritems():
+    if v['salary'] == maxi:
+        print 'outlier name:',k
 
+for k,v in data_dict.iteritems():
+    if v['salary'] > 1000000. and v['bonus'] > 5000000.:
+        if(type(v['salary'])==int):
+            print 'Bandit:', k, v['salary'], v['bonus']
--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -7,14 +7,30 @@ def outlierCleaner(predictions, ages, net_worths):
         residual errors (difference between the prediction
         and the actual net worth).
 
-        Return a list of tuples named cleaned_data where 
+        Return a list of tuples named cleaned_data where
         each tuple is of the form (age, net_worth, error).
     """
-    
+
+    print "Entering outlierCleaner function"
+    print len(predictions), len(ages), len(net_worths)
     cleaned_data = []
 
     ### your code goes here
+    # calculate residual errors
+    error = []
+    for i in range(0,len(predictions)):
+        error.append((predictions[i] - net_worths[i])**2)
 
-    
+    # identify 10 max
+    idx = sorted(range(len(error)), key=error.__getitem__,  reverse=True)
+    print idx
+    print len(idx)
+    for i in range(0,10):
+        pt = idx[i]
+        print i, ages[pt], net_worths[pt], error[pt]
+    for i in range(10,len(idx)):
+        pt = idx[i]
+#        print i, ages[i], net_worths[i], error[i]
+        cleaned_data.append((ages[pt], net_worths[pt], error[pt]))
+    print len(cleaned_data)
     return cleaned_data
-
