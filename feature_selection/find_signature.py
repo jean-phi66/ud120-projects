31a63f29898f17cf078a2b61aa5d639cb17205fb
jean-phi66 <jean.philippe.navarro@gmail.com>
2017-12-01 08:43:57 +0100

    Text learnin and Features selection

--- feature_selection/find_signature.py
+++ feature_selection/find_signature.py
@@ -8,7 +8,7 @@ numpy.random.seed(42)
 ### The words (features) and authors (labels), already largely processed.
 ### These files should have been created from the previous (Lesson 10)
 ### mini-project.
-words_file = "../text_learning/your_word_data.pkl" 
+words_file = "../text_learning/your_word_data.pkl"
 authors_file = "../text_learning/your_email_authors.pkl"
 word_data = pickle.load( open(words_file, "r"))
 authors = pickle.load( open(authors_file, "r") )
@@ -35,9 +35,28 @@ features_test  = vectorizer.transform(features_test).toarray()
 features_train = features_train[:150].toarray()
 labels_train   = labels_train[:150]
 
-
-
-### your code goes here
-
-
-
+# decision tree classifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+clf = DecisionTreeClassifier()
+clf.fit(features_train, labels_train)
+predicted_train = clf.predict(features_train)
+predicted_test = clf.predict(features_test)
+print "## Datasets dimensions ##"
+print "Number of training data points", len(labels_train)
+print "Number of test     data points", len(labels_test)
+print
+print "## Accuracy scores ##"
+print "Accuracy score on training set:", accuracy_score(labels_train, predicted_train)
+print "Accuracy score on test     set:", accuracy_score(labels_test, predicted_test)
+print
+print "## Features importance ##"
+feat_class = clf.feature_importances_
+vocab = vectorizer.get_feature_names()
+cpt =0
+for feat in feat_class:
+    if feat >.2 :
+        print "Feature #:", cpt,"Importance:", feat, "word:", vocab[cpt]
+        impt_feat_num = cpt
+    cpt += 1
+print vocab[impt_feat_num]
--- text_learning/vectorize_text.py
+++ text_learning/vectorize_text.py
@@ -41,25 +41,32 @@ for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
     for path in from_person:
         ### only look at first 200 emails when developing
         ### once everything is working, remove this line to run over full dataset
-        temp_counter += 1
+        #temp_counter += 1
         if temp_counter < 200:
             path = os.path.join('..', path[:-1])
             print path
             email = open(path, "r")
 
             ### use parseOutText to extract the text from the opened email
-
+            stemmed = parseOutText(email)
             ### use str.replace() to remove any instances of the words
             ### ["sara", "shackleton", "chris", "germani"]
-
+            to_be_removed = ["sara", "shackleton", "chris", "germani", "sshacklensf",
+            "cgermannsf"]
+            for item in to_be_removed:
+                stemmed = stemmed.replace(item,'')
             ### append the text to word_data
-
+            word_data.append(stemmed)
             ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
-
+            if name == "sara":
+                from_data.append(0)
+            elif name == "chris":
+                from_data.append(1)
 
             email.close()
 
 print "emails processed"
+print word_data[152]
 from_sara.close()
 from_chris.close()
 
@@ -67,9 +74,10 @@ pickle.dump( word_data, open("your_word_data.pkl", "w") )
 pickle.dump( from_data, open("your_email_authors.pkl", "w") )
 
 
-
-
-
 ### in Part 4, do TfIdf vectorization here
-
-
+from sklearn.feature_extraction.text import TfidfVectorizer
+vectorizer = TfidfVectorizer(stop_words = 'english')
+vectorizer.fit_transform(word_data)
+vocabulary = vectorizer.get_feature_names()
+print "vocabulary length:", len(vocabulary)
+print "vocab word 34597:", vocabulary[34597]
