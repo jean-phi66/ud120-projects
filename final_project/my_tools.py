649b249d17567002aa097df61a78b1882136fa09
jean-phi66 <jean.philippe.navarro@gmail.com>
2017-12-29 14:17:22 +0100

    First submission to Udacity reviewer

--- final_project/my_tools.py
+++ final_project/my_tools.py
@@ -1,79 +1,3 @@
-#!/usr/bin/python
-
-
-# function performing PCA for outliers detection
-# performs PCA and display individual graph with names and color
-# code for poi (red = poi, blue = non_poi)
-def PCA_for_outliers(df, df_data, show_plot=True, n_comp=2, verbose=False):
-    import matplotlib.pyplot as plt
-    import seaborn as sns
-    import pandas as pd
-
-
-    # another reference for PCA https://stackoverflow.com/questions/22984335/recovering-features-names-of-explained-variance-ratio-in-pca-with-sklearn
-    from sklearn.decomposition import PCA as sklearnPCA
-    sklearn_pca = sklearnPCA(n_components=n_comp, whiten = True)
-    Y_sklearn = sklearn_pca.fit_transform(df)
-    if verbose:
-        print 'PCA dimension:', Y_sklearn.shape
-        print "df.columns=", df.columns
-    # display Principal Components vs initial variables
-    # credit to https://stackoverflow.com/questions/22984335/recovering-features-names-of-explained-variance-ratio-in-pca-with-sklearn
-    index_print = list()
-    for i in range(n_comp):
-        index_print.append('PC-' + str(i))
-    if verbose:
-        print index_print
-        print pd.DataFrame(sklearn_pca.components_,columns=df.columns,
-                           index = index_print)
-    # Plot of individuals
-    # PCA and plot reference : http://sebastianraschka.com/Articles/2015_pca_in_3_steps.html#exploratory-visualization
-    # color poi : red non-poi:blue
-    # credit to https://stackoverflow.com/questions/9470056/learning-python-changing-value-in-list-based-on-condition
-    reps = {1.0: 'red', 0.0: 'blue'}
-    col = [reps.get(x,x) for x in df_data['poi']]
-    names = df.index.tolist()
-    with plt.style.context('seaborn-whitegrid'):
-        plt.figure(figsize=(6, 4))
-        plt.scatter(Y_sklearn[:, 0],
-                    Y_sklearn[:, 1], c=col)
-        plt.xlabel('Principal Component 1')
-        plt.ylabel('Principal Component 2')
-        plt.legend(loc='lower center')
-        plt.tight_layout()
-        # annotation
-        cpt =0
-        for name in names:
-            #if col[cpt]=='red':
-            plt.text(Y_sklearn[cpt,0],Y_sklearn[cpt,1], df.index.values[cpt], size=7.5,
-            ha='center', va='top')
-            cpt += 1
-        plt.show()
-    return sklearn_pca
-
-def split_dataset(df):
-    financials = ['salary','deferral_payments', 'total_payments','exercised_stock_options',
-     'bonus','restricted_stock','restricted_stock_deferred','total_stock_value',
-     'expenses', 'other', 'director_fees', 'deferred_income',
-     'long_term_incentive']
-# removed:     loan_advances
-    emails = ['to_messages','shared_receipt_with_poi','from_messages',
-     'from_this_person_to_poi','from_poi_to_this_person', 'interaction_with_poi']
-    df_financial = df
-    df_emails = df
-    for email in emails:
-        if email in df_financial.columns:
-             df_financial = df_financial.drop(email, axis = 1, errors = 'ignore')
-    for financial in financials:
-        if financial in df_emails.columns:
-            df_emails = df_emails.drop(financial, axis = 1, errors = 'ignore')
-    return [df_financial, df_emails]
-
-
-
-def index_in_list(lst, value):
-    return [i for i, x in enumerate(lst) if x==value]
-
 def transform_emails_df(df_in):
     df = df_in.copy()
     df['total_messages'] = df['to_messages'] + df['from_messages']
@@ -87,19 +11,40 @@ def transform_emails_df(df_in):
     df['to_messages_ratio'] = df.to_messages / df.total_messages
     df = df.fillna(0.)
     return df
+
 def transform_financial_df(df_in):
     df = df_in.copy()
     df['salary_ratio'] = df.salary / df.total_payments
     df['bonus_ratio'] = df.bonus / df.total_payments
     df['deferral_payments_ratio'] = df.deferral_payments / df.total_payments
-    #df['exercised_stock_options_ratio'] = df.exercised_stock_options / df.total_payments
-    #df['total_stock_value_ratio'] = df.total_stock_value / df.total_payments
-    #df['restricted_stock_deferred'] = df.restricted_stock_deferred / df.total_payments
     df = df.fillna(0.)
     return df
-#    complete_features_list = ['poi','salary','to_messages','deferral_payments',
-#     'total_payments','exercised_stock_options',
-#     'bonus','restricted_stock','shared_receipt_with_poi','restricted_stock_deferred','total_stock_value',
-#     'expenses', 'loan_advances', 'from_messages', 'other',
-#     'from_this_person_to_poi', 'director_fees', 'deferred_income',
-#     'long_term_incentive', 'email_address', 'from_poi_to_this_person']
+
+def plot_grid_search(cv_results, grid_param_1, grid_param_2, name_param_1, name_param_2):
+    import numpy as np
+    import matplotlib.pyplot as plt
+    import seaborn as sns
+
+    # Get Test Scores Mean and std for each grid search
+    scores_mean = cv_results['mean_test_score']
+    scores_mean = np.array(scores_mean).reshape(len(grid_param_2),len(grid_param_1))
+
+    scores_sd = cv_results['std_test_score']
+    scores_sd = np.array(scores_sd).reshape(len(grid_param_2),len(grid_param_1))
+
+    # Plot Grid search scores
+    plt.close()
+    _, ax = plt.subplots(1,1)
+
+    # Param1 is the X-axis, Param 2 is represented as a different curve (color line)
+    for idx, val in enumerate(grid_param_2):
+        ax.plot(grid_param_1, scores_mean[idx,:], '-o', label= name_param_2 + ': ' + str(val))
+
+    ax.set_title("Grid Search Scores", fontsize=20, fontweight='bold')
+    ax.set_xlabel(name_param_1, fontsize=16)
+    ax.set_ylabel('CV Average Score', fontsize=16)
+    ax.legend(loc="best", fontsize=15)
+    ax.grid('on')
+    plt.savefig('Griv.png')
+    plt.show()
+    return
